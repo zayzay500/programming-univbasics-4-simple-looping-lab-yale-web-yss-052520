babebf8d63afc10b8eff51ee9cffbeecaa411493
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-19 22:39:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,18 @@
 # Write your methods here
+require "pry"
+
+def loop_message_five_times (string)
+  counter = 0 
+  while counter < 5 do
+    puts string
+  end
+end
+
+def loop_message_n_times
+end
+
+def output_array
+end
+
+def return_string_array
+end 
